4a1fa8003e5a2e7a8d081c9f121648c31b6a543f
benjamin-wss <benjamin.wss@gmail.com>
2015-07-08 23:46:49 +0800

    Basic Samples Complete

--- Benjamin.React.MVC/Benjamin.React.MVC/Scripts/Components/CommonComponents.js
+++ Benjamin.React.MVC/Benjamin.React.MVC/Scripts/Components/CommonComponents.js
@@ -24,4 +24,82 @@ Benjamin.Forge.Footer = React.createClass({displayName: "Footer",
     }
 });
 
+Benjamin.Forge.Header = React.createClass({displayName: "Header",
+    render: function() {
+        var linkElementComponents = [];
+
+        for (var i = 0; i < this.props.links.length; i++) {
+            var currentItem = this.props.links[i];
+            linkElementComponents.push(React.createElement(Benjamin.Forge.HeaderLinks, {url: currentItem.url, text: currentItem.text}));
+        }
+
+        var applicationIcon = React.createElement(Benjamin.Forge.ApplicationIcon, {text: "Not Benjamin"});
+
+        return (
+            React.createElement("div", {className: "navbar navbar-inverse navbar-fixed-top"}, 
+                React.createElement("div", {className: "header-content-container container"}, 
+                    React.createElement("div", {className: "navbar-header"}, 
+                        React.createElement("button", {type: "button", className: "navbar-toggle", "data-toggle": "collapse", "data-target": ".navbar-collapse"}, 
+                            React.createElement("span", {className: "icon-bar"}), 
+                            React.createElement("span", {className: "icon-bar"}), 
+                            React.createElement("span", {className: "icon-bar"})
+                        ), 
+                        applicationIcon
+                    ), 
+                    React.createElement("div", {className: "navbar-collapse collapse"}, 
+                        React.createElement("ul", {className: "nav navbar-nav"}, 
+                            linkElementComponents
+                        )
+                    )
+                )
+            )
+        );
+    }
+});
+
+Benjamin.Forge.ApplicationIcon = React.createClass({displayName: "ApplicationIcon",
+    getInitialState: function() {
+        return {
+            css: "navbar-brand",
+            url: "/",
+            text: "Benjamin"
+        };
+    },
+    render: function() {
+
+        if (this.props.css != null && this.props.css != "") {
+            this.state.css = this.props.css;
+        }
+
+        if (this.props.url != null && this.props.url != "") {
+            this.state.url = this.props.url;
+        }
+
+        if (this.props.text != null && this.props.text != "") {
+            this.state.text = this.props.text;
+        }
+
+        return (
+            React.createElement("a", {className: this.state.css, href: this.state.url}, this.state.text)
+        );
+    }
+});
+
+Benjamin.Forge.HeaderLinks = React.createClass({displayName: "HeaderLinks",
+    render: function() {
+        return (
+            React.createElement("li", null, 
+                React.createElement("a", {href: this.props.url}, this.props.text)
+            )
+        );
+    }
+});
+
 React.render(React.createElement(Benjamin.Forge.Footer, {css: "container body-content footer-container"}), $("benjamin_forge_footer").get(0));
+
+var links = [
+    {"url":"/","text":"Home"},
+    {"url":"/Help","text":"API"}
+];
+
+React.render(React.createElement(Benjamin.Forge.Header, {links: links}), $("benjamin_forge_header").get(0));
--- Benjamin.React.MVC/Benjamin.React.MVC/Scripts/ComponentsSrc/CommonComponents.js
+++ Benjamin.React.MVC/Benjamin.React.MVC/Scripts/ComponentsSrc/CommonComponents.js
@@ -24,4 +24,82 @@ Benjamin.Forge.Footer = React.createClass({
     }
 });
 
+Benjamin.Forge.Header = React.createClass({
+    render: function() {
+        var linkElementComponents = [];
+
+        for (var i = 0; i < this.props.links.length; i++) {
+            var currentItem = this.props.links[i];
+            linkElementComponents.push(<Benjamin.Forge.HeaderLinks url={currentItem.url} text={currentItem.text} />);
+        }
+
+        var applicationIcon = <Benjamin.Forge.ApplicationIcon text="Not Benjamin"/>;
+
+        return (
+            <div className="navbar navbar-inverse navbar-fixed-top">
+                <div className="header-content-container container">
+                    <div className="navbar-header">
+                        <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                        </button>
+                        {applicationIcon}
+                    </div>
+                    <div className="navbar-collapse collapse">
+                        <ul className="nav navbar-nav">
+                            {linkElementComponents}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+});
+
+Benjamin.Forge.ApplicationIcon = React.createClass({
+    getInitialState: function() {
+        return {
+            css: "navbar-brand",
+            url: "/",
+            text: "Benjamin"
+        };
+    },
+    render: function() {
+
+        if (this.props.css != null && this.props.css != "") {
+            this.state.css = this.props.css;
+        }
+
+        if (this.props.url != null && this.props.url != "") {
+            this.state.url = this.props.url;
+        }
+
+        if (this.props.text != null && this.props.text != "") {
+            this.state.text = this.props.text;
+        }
+
+        return (
+            <a className={this.state.css} href={this.state.url}>{this.state.text}</a>
+        );
+    }
+});
+
+Benjamin.Forge.HeaderLinks = React.createClass({
+    render: function() {
+        return (
+            <li>
+                <a href={this.props.url}>{this.props.text}</a>
+            </li>
+        );
+    }
+});
+
 React.render(<Benjamin.Forge.Footer css="container body-content footer-container"/>, $("benjamin_forge_footer").get(0));
+
+var links = [
+    {"url":"/","text":"Home"},
+    {"url":"/Help","text":"API"}
+];
+
+React.render(<Benjamin.Forge.Header links={links} />, $("benjamin_forge_header").get(0));
